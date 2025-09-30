444d9216a505019d3d7f47693783c926f8783308
dublin <daydu@mail.uc.edu>
2025-09-29 20:25:02 -0400

    assignment

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,17 @@
 import java.util.Scanner;
-public class Main {
-    public static void main(String[] args) {
+public class Main {//Class ShippingPrice
+    public static void main(String[] args) {//main()
         Scanner in= new Scanner(System.in);
-        double itemPrice= 0;
-        double shipTotal= 0;
-        final double SHIP_COST= 0.02;
-        System.out.println("Enter the price of your item");
-        itemPrice=in.nextDouble();
-        if (itemPrice > 100){
-            System.out.println("Your shipping cost is free ");}
-        else {
-            shipTotal = SHIP_COST * itemPrice;
-            System.out.println("Your shipping cost is $"+shipTotal);
-        }
-    }
-}
+        double itemPrice= 0;//num itemPrice=0
+        double shipTotal= 0;//shipTotal=0
+        final double SHIP_COST= 0.02;//SHIP_COST=0.02
+        System.out.println("Enter the price of your item");//output "Enter the price of your item"
+        itemPrice=in.nextDouble();//input itemPrice
+        if (itemPrice > 100){//if itemPrice >= 100
+            System.out.println("Your shipping cost is free ");}//output "your shipping will be free"
+        else {//else
+            shipTotal = SHIP_COST * itemPrice;//shipTotal = SHIP_COST * itemPrice
+            System.out.println("Your shipping cost is $"+shipTotal);//output "Your shipping will be " +shipPrice
+        }//endif
+    }//return
+}//endClass
